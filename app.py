3ffa732a547162f38c1d72a1b73eeb6b351c8158
Ka Ming Lui <tuk91907@temple.edu>
2023-05-09 12:40:53 -0400

    working code (improvements needed)

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ import base64
 from constants import SHAPE_NAMES, ADDITIONAL_SHAPE_NAMES
 from model import create_model
 from keras.models import load_model
-from keras.preprocessing.image import ImageDataGenerator
-
-
-def augment_image(image, datagen):
-    image_batch = np.expand_dims(image, axis=0)
-    augmented_image = next(datagen.flow(image_batch, batch_size=1))[0]
-    return augmented_image
-
 
 app = Flask(__name__)
 CORS(app)
@@ -30,25 +22,44 @@ def index():
 def recognize_shape():
     img_data = base64.b64decode(request.form['image'])
     image = cv2.imdecode(np.frombuffer(img_data, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray_resized = cv2.resize(gray, (50, 50))
+
+    cv2.imwrite("input_image_debug.png", image)  # Save input image
+
+    # Pre-processing the input image
+    bgr = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+    gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+    alpha = image[:, :, 3]
+
+    # Create a binary image using alpha channel
+    _, binary = cv2.threshold(alpha, 0, 255, cv2.THRESH_BINARY)
+    binary_white_bg = cv2.bitwise_not(binary)
+    gray_shape = cv2.bitwise_and(gray, binary)
+    preprocessed_image = cv2.add(binary_white_bg, gray_shape)
+
+    cv2.imwrite("preprocessed_image_debug.png", preprocessed_image)  # Save preprocessed image
+
+    # Resizing the input image with cv2.INTER_AREA interpolation method
+    gray_resized = cv2.resize(preprocessed_image, (50, 50), interpolation=cv2.INTER_AREA)
+
+    cv2.imwrite("gray_resized_debug.png", gray_resized)  # Save resized grayscale image
+
+    # Predicting the shape
     gray_normalized = gray_resized / 255.0
     gray_normalized = np.expand_dims(gray_normalized, axis=-1)
     gray_normalized = np.expand_dims(gray_normalized, axis=0)
 
-    datagen = ImageDataGenerator(
-        rotation_range=20,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        zoom_range=0.1)
-    augmented_image = augment_image(gray_normalized[0], datagen)
-
-    predictions = model.predict(np.expand_dims(augmented_image, axis=0))
+    predictions = model.predict(gray_normalized)
     shape_idx = np.argmax(predictions)
     shape = shape_names[shape_idx]
     confidence = predictions[0][shape_idx]
 
+    # Post-processing the output
+    confidence_threshold = 0.75
+    if confidence < confidence_threshold:
+        shape = "unknown"
+
     return jsonify({'shape': shape, 'confidence': float(confidence)})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- constants.py
+++ constants.py
@@ -1,8 +1,9 @@
-SHAPE_NAMES = ['circle', 'square', 'triangle']
-ADDITIONAL_SHAPE_NAMES = ['face', 'hourglass', 'hat', 'chair', 'broccoli', 'bowtie', 'traffic light']
+# SHAPE_NAMES = ['circle', 'square', 'triangle']
+SHAPE_NAMES = []
+ADDITIONAL_SHAPE_NAMES = ['duck', 'fence', 'tree', 'bandage', 'camel', 'horse', 'diamond', 'candle', 'postcard', 'monkey']
 
 # Training-related constants
-ADDITIONAL_SHAPE_AMOUNT = 7
+ADDITIONAL_SHAPE_AMOUNT = 10
 INPUT_SHAPE = (50, 50, 1)
 MAX_DRAWINGS = 5000
 TEST_SIZE = 0.2
